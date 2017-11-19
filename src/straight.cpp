a9de9a2e36a677002f1198b3ad59fb2970d76170
woombat84 <mblank16@student.aau.dk>
2017-11-19 01:06:02 +0100

    Now it is posibel to see robot coordinates

--- src/straight.cpp
+++ src/straight.cpp
@@ -1,12 +1,15 @@
-#include <ros/ros.h>
-#include <geometry_msgs/Twist.h>
-#include <stdlib.h>
-#include <turtlesim/Pose.h>
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include <sstream>
+#include "turtlesim/Pose.h"
 
 
 using namespace std;
 
-
+ros::Publisher pub;
+ros::Subscriber pose_subscriber;
+turtlesim::Pose turtlesim_pose;
+const double PI =3.14159265359;
 float xa = 2; // x coordinate  for waypoint
 float ya = 2; // y coordinate  for waypoint
 float xb = 8; // x coordinate  for end
@@ -16,28 +19,21 @@ float yr; // y coordinate of the robot
 float xba = 0; // AB x vector
 float yba = 0; // AB y vector
 float ir = 0;  // angular
-
 int lookahead = 2; //
-
 float lx = 0; // lookahead
 float ly = 0; // lookahead
 
-float Pose(const turtlesim::Pose::ConstPtr& msg)
-{
-  float a = msg->x;
-  float b = msg->y;
-  xr = a;
-  yr = b;
 
-}
+
+void poseCallback(const turtlesim::Pose::ConstPtr & msg);
 
   int main(int argc, char**argv){
-  ros::init(argc, argv, "publish_velocity");
 
-  ros::NodeHandle nh;
+  ros::init(argc, argv, "helmsman");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe<turtlesim::Pose>("turtle1/pose", 10, Pose);
-  ros::Publisher  pub = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 10);
+
+  pose_subscriber = n.subscribe("/turtle1/pose",10 , poseCallback);
+  pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
   ros::Rate rate(10);
   while(ros::ok()){
 
@@ -46,17 +42,24 @@ float Pose(const turtlesim::Pose::ConstPtr& msg)
     msg.linear.x = 0.1;
     msg.angular.z = 0.1;
     pub.publish(msg);
-		
-	
-    cout << "robot x:"<< xr <<"robot y:" << yr << endl;
-	
-	
+
+cout<<"Send command:"<<" linear="<<msg.linear.x<<" angular="<<msg.angular.z<< "/nrobot x: "<<xr<<"robot y: " << yr<<"looking i direction: "<<ir*180/PI <<endl;
+ros::spinOnce();
 }
 
 
-  ROS_INFO_STREAM("Sending velocity command:"<<" linear="<<msg.linear.x<<" angular="<<msg.angular.z);
+
 
   rate.sleep();
   }
 return 0;
-}
+};
+
+
+void poseCallback(const turtlesim::Pose::ConstPtr & msg)
+{
+      xr = msg->x;
+      yr = msg->y;
+      ir = msg->theta;
+
+};
